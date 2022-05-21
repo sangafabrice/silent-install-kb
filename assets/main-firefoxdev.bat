40e5a97989017972e58e1464dad55a26a6d6669f
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-05-21 05:01:00 +0200

    modify firefox updater

--- assets/main-firefoxdev.bat
+++ assets/main-firefoxdev.bat
@@ -13,7 +13,7 @@ Set shim=%PROFILE_DRIVE_PATH%\firefoxdev.bat
 Set compare=2
 Set version=
 Set link=
-Call :get-download-info version link
+Call dl-info\GetFrom-Link.bat Firefox-Dev version link > Nul
 If EXIST "%app%" Call compare-version-cli.bat "%version:b=.%" "%ComSpec%" "/c Echo %FIREFOX_DEV_VERSION:b=.%" compare
 If "%compare%"=="2" Call install-gecko-setup.bat "%version%" "%link%" "%app%" "%~f1" > Nul
 If EXIST "%app%" (
@@ -24,22 +24,6 @@ EndLocal
 Call "%PROFILE_DRIVE_PATH%\mod_firefoxdev.bat" > Nul 2>&1
 PopD
 GoTo :EOF
-
-:get-download-info 1:=version 2:=link
-    For /F "Tokens=2 Delims=? " %%L In ('^
-        Curl --url "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=win64"^
-             --head ^
-             --request  "GET"^
-             --header "User-Agent: winhttp"^
-             --silent ^|^
-        FindStr /I /B "Location:"^
-    ') Do (
-        Set %~1=%%~nL
-        Set %~1=!%~1:Firefox%%20Setup%%20=!
-        Set %~2=%%~L
-        Set %~2=!%~2:%%=%%%%!
-    )
-    GoTo :EOF
 
 :write-to-profile
     Echo Set FIREFOX_DEV_VERSION=%version%> "%PROFILE_DRIVE_PATH%\mod_firefoxdev.bat"
