0eeeb010cf8901be82e5b3adb2c6c2dfc1c20003
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-05-27 08:59:47 +0200

    remove echo line

--- assets/resource/wt/config.bat
+++ assets/resource/wt/config.bat
@@ -3,12 +3,14 @@
 :Main
 
 PushD "%~dp0"
+If /I "/SkipCheck"=="%~1" Goto Skipped
 Reg Query HKCU\SOFTWARE\Classes\CustomUI /V "WTSettings" /Reg:64 > Nul 2>&1 &&^
 PopD &&^
 GoTo :EOF
+:Skipped
 Reg Add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked" /V "{9F156763-7844-4DC4-B2B1-901F640F5155}" /D "" /F /Reg:64 > Nul 2>&1
 Call :WithSettingsJson "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" wt-profile-list.json
-Reg Add HKCU\SOFTWARE\Classes\CustomUI /V "WTSettings" /D "" /Reg:64 > Nul 2>&1
+Reg Add HKCU\SOFTWARE\Classes\CustomUI /V "WTSettings" /D "" /F /Reg:64 > Nul 2>&1
 PopD
 GoTo :EOF
 
